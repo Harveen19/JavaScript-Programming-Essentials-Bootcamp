210c4fb42975c29df9c205d1f3aa9be3836bf88f
Harveen19 <harveenkaur1912@gmail.com>
2022-12-07 20:08:33 +0530

    Add all my files

---
+++ calculator.js
@@ -0,0 +1,10 @@
+
+  var numOne=12, numTwo=10, res;
+  res = numOne + numTwo;
+  document.write("Add = " + res );
+  res = numOne - numTwo;
+  document.write("Subtract = " + res );
+  res = numOne * numTwo;
+  document.write("Multiply = " + res );
+  res = numOne/numTwo;
+  document.write("Divide = " + res );
